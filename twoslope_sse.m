8047a7080951a188fb7a32387c40f30d40c38b54
Min Ding <dingmin@alum.mit.edu>
2022-02-22 10:47:29 +0800

    Delete twoslope_sse.m

--- twoslope_sse.m
+++
@@ -1,9 +0,0 @@
-    function [sse] = twoslope_sse(params,Dc,cba,b)
-        Dmin = params(1);
-        a = params(2);
-        FittedCurve = Dc*0;
-        s = Dc<=Dmin; FittedCurve(s) = b;
-        s = Dc>Dmin; FittedCurve(s) = a*(Dc(s)-Dmin)+b;
-        ErrorVector = FittedCurve - cba;
-        sse = sum(ErrorVector .^ 2);
-    end
